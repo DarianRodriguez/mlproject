3b86f22a3aa04864dbf9717e17b92c4bc6e91ca1
Darian Rodriguez <uceedmr@ucl.ac.uk>
2024-11-30 12:37:08 -0400

    data ingestion

--- src/logger.py
+++ src/logger.py
@@ -3,11 +3,12 @@ import os
 from datetime import datetime
 
 LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
+logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE) #/current/working/directory/logs/11_29_2024_10_45_30.log
 os.makedirs(logs_path,exist_ok=True)
 
-LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
+LOG_FILE_PATH= os.path.join(logs_path,LOG_FILE)
 
+# Configuring the Logging Modulem, logs only log messages of severity INFO or higher
 logging.basicConfig(
     filename=LOG_FILE_PATH,
     format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
